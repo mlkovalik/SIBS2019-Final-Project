2a380f6f5e5a7037b54abeb2d2301bf9c6904f14
Isabel Larsen <isabellarsen207@gmail.com>
2019-07-03 21:50:26 -0400

    Add crosstables, plot frequencies rather than counts

--- Exploring.R
+++ Exploring.R
@@ -1,6 +1,7 @@
 # Libraries ---------------------------------------------------------------
 library(dplyr)
 library(ggplot2)
+library(gmodels)
 
 
 # Basic Tables & Plots ----------------------------------------------------
@@ -93,148 +94,165 @@ table(drugConsumption$VolatileSubstanceAbuse)
 plot(drugConsumption$VolatileSubstanceAbuse)
 
 
-# Looking at Frequencies --------------------------------------------------
-
-#Countries
-  countryFreq <- drugConsumption %>%
-    group_by(Country) %>%
-    summarise(Counts = n())
-
-  ggplot(countryFreq, aes(Country, Counts)) +
-    geom_bar(stat="identity") +
-    geom_text(aes(label=Counts), vjust=-0.3)
 
 # Graphical Comparisons ---------------------------------------------------
   
 #Alcohol
 
   #Alcohol consumption ~ gender
+  ftable(xtabs(~Alcohol+Gender, data=drugConsumption))  
+  CrossTable(drugConsumption$Alcohol, drugConsumption$Gender)
+  
     ggplot(drugConsumption, aes(Alcohol, color=Gender, fill=Gender)) +
-      geom_bar(position="dodge") +
-      labs(title="Alcohol Consumption by Gender", x="Latest Alcohol Consumption", fill="Gender") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position="dodge") +
+      labs(title="Alcohol Consumption by Gender", x="Latest Alcohol Consumption", y="Percent", fill="Gender") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
     ggplot(drugConsumption, aes(Alcohol)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Gender) +
-      labs(title="Alcohol Consumption by Gender", x="Latest Alcohol Consumption") +
+      labs(title="Alcohol Consumption by Gender", x="Latest Alcohol Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
   #Alcohol consumption ~ age
+    ftable(xtabs(~Alcohol+Age, data=drugConsumption))
+    CrossTable(drugConsumption$Alcohol, drugConsumption$Age)
+    
     ggplot(drugConsumption, aes(Alcohol, fill=Age)) +
-      geom_bar(position = "dodge") +
-      labs(title="Alcohol Consumption by Age", x="Latest Alcohol Consumption", fill="Age Group") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
+      labs(title="Alcohol Consumption by Age", x="Latest Alcohol Consumption", y="Percent", fill="Age Group") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal")
     
     ggplot(drugConsumption, aes(Alcohol)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Age) +
-      labs(title="Alcohol Consumption by Age", x="Latest Alcohol Consumption") +
+      labs(title="Alcohol Consumption by Age", x="Latest Alcohol Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
-            legend.box = "horizontal")
+            legend.box = "horizontal") 
     
   #Alcohol consumption ~ country
+    ftable(xtabs(~Alcohol+Country, data=drugConsumption))  
+    CrossTable(drugConsumption$Alcohol, drugConsumption$Country)
+    
     ggplot(drugConsumption, aes(Alcohol, fill=Country)) +
-      geom_bar(position = "dodge") +
-      labs(title="Alcohol Consumption by Country", x="Latest Alcohol Consumption", fill="Country") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
+      labs(title="Alcohol Consumption by Country", x="Latest Alcohol Consumption", y="Percent", fill="Country") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal")
     
     ggplot(drugConsumption, aes(Alcohol)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Country) +
-      labs(title="Alcohol Consumption by Country", x="Latest Alcohol Consumption") +
+      labs(title="Alcohol Consumption by Country", x="Latest Alcohol Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal")
 
 #Cannabis
     
   #Cannabis consumption ~ gender
+    ftable(xtabs(~Cannabis+Gender, data=drugConsumption))  
+    CrossTable(drugConsumption$Cannabis, drugConsumption$Gender)
+    
     ggplot(drugConsumption, aes(Cannabis, fill=Gender)) +
-      geom_bar(position="dodge") +
-      labs(title="Cannabis Consumption by Gender", x="Latest Cannabis Consumption", fill="Gender") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position="dodge") +
+      labs(title="Cannabis Consumption by Gender", x="Latest Cannabis Consumption", y="Percent", fill="Gender") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
     ggplot(drugConsumption, aes(Cannabis)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Gender) +
-      labs(title="Cannabis Consumption by Gender", x="Latest Cannabis Consumption") +
+      labs(title="Cannabis Consumption by Gender", x="Latest Cannabis Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
   #Cannabis consumption ~ age
+    ftable(xtabs(~Cannabis+Age, data=drugConsumption))  
+    CrossTable(drugConsumption$Cannabis, drugConsumption$Age)
+    
     ggplot(drugConsumption, aes(Cannabis, fill=Age)) +
-      geom_bar(position="dodge") +
-      labs(title="Cannabis Consumption by Age", x="Latest Cannabis Consumption", fill="Age Group") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position="dodge") +
+      labs(title="Cannabis Consumption by Age", x="Latest Cannabis Consumption", y="Percent", fill="Age Group") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
     ggplot(drugConsumption, aes(Cannabis)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Age) +
-      labs(title="Cannabis Consumption by Age", x="Latest Cannabis Consumption") +
+      labs(title="Cannabis Consumption by Age", x="Latest Cannabis Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
   #Cannabis consumption ~ country
+    ftable(xtabs(~Cannabis+Country, data=drugConsumption))  
+    CrossTable(drugConsumption$Cannabis, drugConsumption$Country)
+    
     ggplot(drugConsumption, aes(Cannabis, fill=Country)) +
-      geom_bar(position="dodge") +
-      labs(title="Cannabis Consumption by Country", x="Latest Cannabis Consumption", fill="Age Group") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position="dodge") +
+      labs(title="Cannabis Consumption by Country", x="Latest Cannabis Consumption", y="Percent", fill="Age Group") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
     ggplot(drugConsumption, aes(Cannabis)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Country) +
-      labs(title="Cannabis Consumption by Country", x="Latest Cannabis Consumption") +
+      labs(title="Cannabis Consumption by Country", x="Latest Cannabis Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
 #Nicotine
     
   #Nicotine consumption ~ gender
+    ftable(xtabs(~Nicotine+Gender, data=drugConsumption))  
+    CrossTable(drugConsumption$Nicotine, drugConsumption$Gender)
+    
     ggplot(drugConsumption, aes(Nicotine, fill=Gender)) +
-      geom_bar(position="dodge") +
-      labs(title="Nicotine Consumption by Gender", x="Latest Nicotine Consumption", fill="Gender") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position="dodge") +
+      labs(title="Nicotine Consumption by Gender", x="Latest Nicotine Consumption", y="Percent", fill="Gender") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
     ggplot(drugConsumption, aes(Nicotine)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Gender) +
-      labs(title="Nicotine Consumption by Gender", x="Latest Nicotine Consumption") +
+      labs(title="Nicotine Consumption by Gender", x="Latest Nicotine Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
   #Nicotine consumption ~ age
+    ftable(xtabs(~Nicotine+Age, data=drugConsumption))  
+    CrossTable(drugConsumption$Nicotine, drugConsumption$Age)
+    
     ggplot(drugConsumption, aes(Nicotine, fill=Age)) +
-      geom_bar(position="dodge") +
-      labs(title="Nicotine Consumption by Age", x="Latest Nicotine Consumption", fill="Age Group") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position="dodge") +
+      labs(title="Nicotine Consumption by Age", x="Latest Nicotine Consumption", y="Percent", fill="Age Group") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
     ggplot(drugConsumption, aes(Nicotine)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Age) +
-      labs(title="Nicotine Consumption by Age", x="Latest Nicotine Consumption") +
+      labs(title="Nicotine Consumption by Age", x="Latest Nicotine Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
   #Nicotine consumption ~ country
+    ftable(xtabs(~Nicotine+Country, data=drugConsumption))  
+    CrossTable(drugConsumption$Nicotine, drugConsumption$Country)
+    
     ggplot(drugConsumption, aes(Nicotine, fill=Country)) +
-      geom_bar(position="dodge") +
-      labs(title="Nicotine Consumption by Country", x="Latest Nicotine Consumption", fill="Age Group") +
+      geom_bar(aes(y=(..count..)/sum(..count..), position="dodge")) +
+      labs(title="Nicotine Consumption by Country", x="Latest Nicotine Consumption", y="Percent", fill="Age Group") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
     ggplot(drugConsumption, aes(Nicotine)) +
-      geom_bar(position = "dodge") +
+      geom_bar(aes(y=(..count..)/sum(..count..)), position = "dodge") +
       facet_grid(. ~ Country) +
-      labs(title="Nicotine Consumption by Country", x="Latest Nicotine Consumption") +
+      labs(title="Nicotine Consumption by Country", x="Latest Nicotine Consumption", y="Percent") +
       theme(axis.text.x = element_text(angle = 90), plot.title = element_text(size = 20, face="bold.italic"), legend.position="bottom", 
             legend.box = "horizontal") 
     
